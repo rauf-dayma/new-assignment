5375dd3d949e809037196239ba373c2b71708d14
rauf-dayma <daymarauf@gmail.com>
2025-01-01 18:41:06 +0530

    final commit

--- backend/api/script.js
+++ backend/api/script.js
@@ -5,28 +5,10 @@ import { uploadVideoRoute } from "./routes/video.js";
 
 const app = express();
 
-// CORS middleware for preflight requests
-app.options('*', cors());
+app.use(cors())
 
-// CORS configuration
-app.use(cors({
-  origin: ["http://localhost:5173", "https://new-assignment-delta.vercel.app/"], // Make sure to replace with your actual frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  Credential : true
-}));
-
-app.use((req, res, next) => {
-  console.log('Request headers:', req.headers);
-  res.setHeader('Access-Control-Allow-Origin', '*');  // Temporary to allow any origin
-  next();
-});
-
-
-// Express setup
 app.use(express.json());
 
-// MongoDB connection
 const dbUri = "mongodb+srv://raufdayma123:raufdayma-123@cluster0.0jbpr.mongodb.net/neoflekeTaskDb?retryWrites=true&w=majority";
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -34,13 +16,10 @@ const db = mongoose.connection;
 db.on("open", () => console.log("db Connection successful"));
 db.on("error", () => console.log("Connection is not successful"));
 
-// Your routes
-uploadVideoRoute(app);
-
-app.get('/test', (req, res) => {
-  res.json({ message: "CORS is working!" });
+app.listen(2100, () => {
+  console.log("Server is running on port 2100");
 });
 
+uploadVideoRoute(app);
 
-// Export app for Vercel
 export default app;
