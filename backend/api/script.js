4ede0d584fc213ffe6c4cd1840f75c8f041829f9
rauf-dayma <daymarauf@gmail.com>
2025-01-02 16:05:39 +0530

    second last commit

--- backend/api/script.js
+++ backend/api/script.js
@@ -9,7 +9,6 @@ app.use(cors())
 
 app.use(express.json());
 
-
 const dbUri = "mongodb+srv://raufdayma123:raufdayma-123@cluster0.0jbpr.mongodb.net/neoflekeTaskDb?retryWrites=true&w=majority";
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -17,6 +16,10 @@ const db = mongoose.connection;
 db.on("open", () => console.log("db Connection successful"));
 db.on("error", () => console.log("Connection is not successful"));
 
+
+app.listen(2100, () => {
+  console.log(`Server is running on port 2100}`);
+});
 
 uploadVideoRoute(app);
 
