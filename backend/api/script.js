da92792c02472e0a77fcf5c53c3b4cef90b6c65a
rauf-dayma <daymarauf@gmail.com>
2025-01-02 16:55:11 +0530

    final commit

--- backend/api/script.js
+++ backend/api/script.js
@@ -2,8 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { uploadVideoRoute } from "./routes/video.js";
+import path from 'path';  
 
 const app = express();
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 
 app.use(cors())
 
